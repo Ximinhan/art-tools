ec8ca5eff903bd2349cb731b312540a60c2aaf52
ximhan <ximhan@redhat.com>
2023-08-11 20:05:15 +0800

    add check

--- doozer/tests/test_exectools.py
+++ doozer/tests/test_exectools.py
@@ -73,7 +73,7 @@ class RetryTestCase(IsolatedAsyncioTestCase):
 
     def test_return(self):
         """
-        Verify that the retry task return value is passed back out faithfully.
+        Verify that the retry task return value is passed back out faithfully.test
         """
         obj = {}
 
