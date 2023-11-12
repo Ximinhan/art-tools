6909076e55a2cecc32366f0b0641acc1d54998e1
ximhan <ximhan@redhat.com>
2023-11-12 20:40:45 +0800

    add api

--- app/main.py
+++ app/main.py
@@ -12,15 +12,16 @@ def read_root():
 
 @app.get("/image/{image_name}")
 def get_images(image_name: str):
-    url = "https://api.github.com/repos/openshift-eng/ocp-build-data/branches?per_page=100"
-    response = requests.get(url)
-    if response.status_code == 200:
-        branches = [branch["name"] for branch in response.json()]
-        pattern = re.compile(r"openshift-\d+\.\d+\d+$")
-        release_branches = [branch for branch in branches if pattern.match(branch)]
-    else:
-        print(f"Error fetching branches: {response.status_code}")
-        return {}
+    # url = "https://api.github.com/repos/openshift-eng/ocp-build-data/branches?per_page=100"
+    # response = requests.get(url)
+    # if response.status_code == 200:
+    #     branches = [branch["name"] for branch in response.json()]
+    #     pattern = re.compile(r"openshift-\d+\.\d+\d+$")
+    #     release_branches = [branch for branch in branches if pattern.match(branch)]
+    # else:
+    #     print(f"Error fetching branches: {response.status_code}")
+    #     return {}
+    release_branches = ["openshift-4.15", "openshift-4.14", "openshift-4.13", "openshift-4.12", "openshift-4.11", "openshift-4.10"]
     ret = {}
     for branch in release_branches:
         response = requests.get(f"https://api.github.com/repos/openshift-eng/ocp-build-data/contents/images/{image_name}.yml?ref={branch}")
